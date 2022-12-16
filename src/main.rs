1e66ffdf4c5182d0c127cddd73c2a00029fa39b6
StormLight14 <stormpower44@protonmail.com>
2022-12-15 19:32:21 -0700

    change into 2 teamed counter
    add 2 more button types, and there are paragraph elements showing teams now

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use yew::prelude::*;
 enum Msg {
-    AddOne,
-    RemoveOne,
+    Team1AddOne,
+    Team1RemoveOne,
+    Team2AddOne,
+    Team2RemoveOne,
 }
 
 struct CounterComponent {
-    count: i64,
+    team1_count: i64,
+    team2_count: i64,
 }
 
 impl Component for CounterComponent {
@@ -13,30 +16,48 @@ impl Component for CounterComponent {
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { count: 0 }
+        Self { team1_count: 0 , team2_count: 0}
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => {
-                self.count += 1;
-                true
+            Msg::Team1AddOne => {
+                self.team1_count += 1;
+                return true;
             }
-            Msg::RemoveOne => {
-                self.count -= 1;
-                true
+            Msg::Team1RemoveOne => {
+                self.team1_count -= 1;
+                return true;
+            }
+            Msg::Team2AddOne => {
+                self.team2_count += 1;
+                return true;
+            }
+            Msg::Team2RemoveOne => {
+                self.team2_count -= 1;
+                return true;
             }
         }
+
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
         html! {
+        <>
           <div class="container">
-            <p>{self.count}</p>
-            <button onclick={link.callback(|_| Msg::AddOne)}>{"+1"}</button>
-            <button onclick={link.callback(|_| Msg::RemoveOne)}>{"-1"}</button>
+            <p>{"Team 1 Score: "} {self.team1_count}</p>
+            <button onclick={link.callback(|_| Msg::Team1AddOne)}>{"+1"}</button>
+            <button onclick={link.callback(|_| Msg::Team1RemoveOne)}>{"-1"}</button>
+            <p>{"Team 2 Score: "} {self.team2_count}</p>
+            <button onclick={link.callback(|_| Msg::Team2AddOne)}>{"+1"}</button>
+            <button onclick={link.callback(|_| Msg::Team2RemoveOne)}>{"-1"}</button>
           </div>
+          <footer>
+          <p>{"StormLight14 on GitHub"}</p>
+          </footer>
+          </>
+          
         }
     }
 }
